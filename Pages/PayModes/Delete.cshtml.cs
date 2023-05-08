fe5ed05280421758268125bd056ded888c6abc64
Miguel Angel Martinez Gomez <miguelmartinezgomez27@gmail.com>
2023-05-08 14:41:53 -0500

    Delete Pay mode

---
+++ Pages/PayModes/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWeb.Data;
+using SupermarketWeb.Models;
+
+namespace SupermarketWeb.Pages.PayModes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public PayMode PayMode { get; set; } = default!;
+
+        public async Task<IActionResult> OngetAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+
+            var payMode = await _context.PayModes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (payMode == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                PayMode = payMode;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+
+            var payMode = await _context.PayModes.FindAsync(id);
+
+            if (payMode != null)
+            {
+                PayMode = payMode;
+                _context.PayModes.Remove(PayMode);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+
+        }
+    }
+}
